16d7f1cc86faa7d825d823cd8cf19c2fafd41a15
omnipotentpenguin <adammartin@hotmail.co.uk>
2020-02-28 11:30:21 +0000

    set up routes js

--- mini-mern/src/App.js
+++ mini-mern/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import SignUp from './Components/SignUp';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <SignUp></SignUp>
+      <Router></Router>
     </div>
   );
 }
---
+++ mini-mern/src/Components/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+export default function Routes(){
+    return(
+        <Router>
+            <Route>
+                
+            </Route>
+        </Router>
+    )
+}

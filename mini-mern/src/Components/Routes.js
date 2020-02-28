eb58bb6dda1bdfdccc2f1944bac1077eaa1edf25
RebekahZoe <rebekah.zoe.radcliffe@gmail.com>
2020-02-28 14:57:30 +0000

    potentially axios working

--- mini-mern/src/Components/Routes.js
+++ mini-mern/src/Components/Routes.js
@@ -10,7 +10,7 @@ export default function Routes() {
         <Router>
             <Route exact path='/' component={SignIn}></Route>
             <Route exact path='/signup' component={SignUp}></Route>
-            <Route exact path="/welcome" component={WelcomePage}/>
+            <Route exact path="/welcome/:username" component={WelcomePage}/>
             <Footer></Footer>
 
         </Router>

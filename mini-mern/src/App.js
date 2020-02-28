25dba3c7b7c18895e58a782daf5e4c93b4a97788
omnipotentpenguin <adammartin@hotmail.co.uk>
2020-02-28 11:17:38 +0000

    stage one page structure

--- mini-mern/src/App.js
+++ mini-mern/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <SignUp></SignUp>
     </div>
   );
 }
---
+++ mini-mern/src/Components/SignUp.js
@@ -0,0 +1,8 @@
+import { React } from 'react';
+import InputBar from './InputBar';
+
+export default function SignUp(){
+    return(
+        <InputBar></InputBar>
+    )
+}
